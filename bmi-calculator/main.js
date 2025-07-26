22b29bfbc8c25b6b09846ca663247e8ffd1995d2
hariom24777 <hariompatidar751@gmail.com>
2025-07-26 12:51:19 +0530

    category list updated

--- bmi-calculator/main.js
+++ bmi-calculator/main.js
@@ -62,9 +62,9 @@ form.addEventListener("submit", (e) => {
     const bmi = weight / (height / 100) ** 2;
     let category = "";
 
-    if (bmi < 18.5) category = "Underweight";
+    if (bmi < 18.6) category = "Underweight";
     else if (bmi < 24.9) category = "Normal weight";
-    else if (bmi < 29.9) category = "Overweight";
+    else if (bmi < 28.9) category = "Overweight";
     else category = "Obese";
 
     results.innerHTML = `
